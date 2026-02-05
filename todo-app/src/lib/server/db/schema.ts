ec77a2a724d03daea436d28565554adb2ae44097
Cambry-SafeStreets <cpartridge@safestreets.com>
2026-02-05 11:20:14 -0700

    Drizzle User + Todo schema

--- todo-app/src/lib/server/db/schema.ts
+++ todo-app/src/lib/server/db/schema.ts
@@ -1,2 +1,25 @@
-import { pgTable, serial, varchar, text } from 'drizzle-orm/pg-core';
-import { Relations } from 'drizzle-orm';
+import { pgTable, serial, varchar, text, integer } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
+
+export const Users = pgTable('users', {
+    id: serial('id').primaryKey(),
+    username: varchar( "username", { length: 15}).notNull(),
+    password: text('password').notNull()
+})
+
+export const Todos = pgTable('todos', {
+    id: serial('id').primaryKey(),
+    description: varchar("description", {length: 100}).notNull(),
+    userid: integer('user_id').references(() => Users.id).notNull(),
+})
+
+export const userRelations = relations( Users, ({many}) => ({
+    todos: many(Todos)
+}))
+
+export const postRelations = relations(Todos, ({one}) => ({
+    author: one( Users, {
+        fields: [Todos.userid],
+        references: [Users.id],
+    })
+}))
